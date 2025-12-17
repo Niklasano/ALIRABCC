d4819c188cf9149eee1d2907095e800bc480784c
Niklasano <n.pebernet@gmail.com>
2025-12-17 15:53:54 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import BeloteApp from "@/components/BeloteApp";
 import ThemeToggle from "@/src/components/ThemeToggle";
 
 export default function Index() {
-  const [showWelcome, setShowWelcome] = useState(true);
-
   return (
     <>
       <ThemeToggle />
-      <WelcomeDialog 
-        open={showWelcome} 
-        onClose={() => setShowWelcome(false)} 
-      />
       <div className="flex justify-center mb-2">
         <img 
           src="/lovable-uploads/88d78836-a013-4b12-bd37-dea6ec9e1b44.png" 
