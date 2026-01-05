89ade20e36693de85f8a4a537ddc9e5a5860a77f
Niklasano <n.pebernet@gmail.com>
2026-01-05 14:51:35 +0100

    build app android

---
+++ android/app/src/main/java/com/alirabcc/belote/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alirabcc.belote;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alirabcc.belote',
+  appName: 'ALIRABCC',
+  webDir: 'dist'
+};
+
+export default config;
