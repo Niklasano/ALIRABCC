c00643399869aff6e2234863537d9015f0d64d4e
Niklasano <n.pebernet@gmail.com>
2026-01-08 13:04:56 +0100

    Revert "à annuler"
    This reverts commit 438d080dba834eeb389e6da657f9b0ae3142b2bd.

--- src/components/BeloteApp.tsx
+++ src/components/BeloteApp.tsx
@@ -353,9 +353,8 @@ if (contratE1Val > 0) {
 }
 
 // --- MAINTENANT on calcule ecartE1 et ecartE2 avec les pénalités ---
-
-	ecartE1 = ecartBaseE1;
-	ecartE2 = ecartBaseE2;
+let ecartE1 = ecartBaseE1;
+let ecartE2 = ecartBaseE2;
 
   if (remarqueE1Display === "Capot non annoncé" || remarqueE1Display === "Vous êtes nuls") {
     ecartE1 = 500 - contratE1Val - realiseE1Final;
